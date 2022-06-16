d71fab680e4a7a3c664487d60389cdfe1f324f81
Gustavo <gust.bersan@gmail.com>
2022-06-15 21:29:48 -0300

    Criando m´[etodo construtor para a classe Card

--- Cards/src/Classes/Card.cs
+++ Cards/src/Classes/Card.cs
@@ -3,5 +3,15 @@ namespace Cards
     public abstract class Card
     {
         private int Id { get; set; }
+        private TypeCard Value { get; set; }
+        private string Suits { get; set; }
+
+        private Card(int id, TypeCard value, string suits)
+        {
+            this.Id = id;
+            this.Value = value;
+            this.Suits = suits;
+        }
+        private Card() { }
     }
 }
