ac8ca70ed30e1d4f6d28056b47700512e8c5efb0
Gustavo <gust.bersan@gmail.com>
2022-06-20 22:15:50 -0300

    Retirando atributo Id
    O atributo não será utilizado no decorrer do projeto

--- Cards/src/Classes/Card.cs
+++ Cards/src/Classes/Card.cs
@@ -2,13 +2,11 @@ namespace Cards
 {
     public abstract class Card
     {
-        private int Id { get; set; }
         private TypeCard Value { get; set; }
         private string Suits { get; set; }
 
-        private Card(int id, TypeCard value, string suits)
+        private Card(TypeCard value, string suits)
         {
-            this.Id = id;
             this.Value = value;
             this.Suits = suits;
         }
