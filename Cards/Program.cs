a684259b0efe6c56020e6a5cc1fda46c1c7876a2
Gustavo <gust.bersan@gmail.com>
2022-06-21 23:09:13 -0300

    Criando método de exibição de opções para usuário

--- Cards/Program.cs
+++ Cards/Program.cs
@@ -6,7 +6,25 @@ namespace Cards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            UserOptions();
+        }
+
+        private static string UserOptions()
+        {
+            Console.WriteLine("Vizualizador de cartas!!!\n------------------------------");
+            Console.WriteLine("1 - Visualizar todas as cartas");
+            Console.WriteLine("2 - Visualizar cartas de Ouros");
+            Console.WriteLine("3 - Visualizar cartas de Espadas");
+            Console.WriteLine("4 - Visualizar cartas de Copas");
+            Console.WriteLine("5 - Visualizar cartas de Paus");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+            Console.Write("Escolha uma das opções acima: ");
+
+            string userOption = Console.ReadLine().ToUpper();
+            return userOption;
         }
     }
 }
